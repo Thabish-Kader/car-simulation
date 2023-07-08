b2339d9baad639ac90914077e0190392a61a3726
Thabish <kadertabish2@gmail.com>
2023-07-08 09:51:05 +0530

    floor
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/components/Platform.tsx
+++ app/components/Platform.tsx
@@ -1,6 +1,9 @@
 "use client";
-
 import { FC } from "react";
+import * as THREE from "three";
+
+// instantiated in the top for optimization
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
 type PlatformProps = {
 	position?: [number, number, number];
@@ -9,8 +12,12 @@ type PlatformProps = {
 export const Platform: FC<PlatformProps> = ({ position = [0, 0, 0] }) => {
 	return (
 		<group position={position}>
-			<mesh position={[0, -0.1, 0]} receiveShadow>
-				<boxGeometry args={[4, 0.2, 4]} />
+			<mesh
+				geometry={boxGeometry}
+				scale={[4, 0.2, 4]}
+				position={[0, -0.1, 0]}
+				receiveShadow
+			>
 				<meshStandardMaterial color="limegreen" />
 			</mesh>
 		</group>
